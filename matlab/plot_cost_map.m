0d39c72a4ea7dc9f358ec8dab459e46f46aa5f6c
Jim Mainprice <mainprice@gmail.com>
2014-01-17 16:58:13 -0500

    Add new points to the cost map

--- matlab/plot_cost_map.m
+++ matlab/plot_cost_map.m
@@ -1,25 +1,27 @@
 clear; clc;
 
-figure
-
 % cost_map = load('cost_map_0.txt')
 % cost_map = load('cost_map_64.txt');
- cost_map = load('matlab/cost_map_derv_0_64.txt');
+% cost_map = load('matlab/cost_map_feat_64.txt');
+% cost_map = load('matlab/cost_map_derv_1_64.txt');
 % cost_map = load('matlab/cost_map_derv_1_64.txt');
 
-[X,Y] = meshgrid([-40:0.800001:40]);
-Z = cost_map;
-contour3(X,Y,Z,30)
-surface(X,Y,Z,'EdgeColor',[.8 .8 .8],'FaceColor','none')
-grid off
-Tcam = [135 -70];
-view(Tcam);
-colormap cool
+HeatMap(load('matlab/cost_map_64.txt'));
+HeatMap(load('matlab/cost_map_feat_64.txt'));
+HeatMap(load('matlab/cost_map_derv_0_64.txt'));
+HeatMap(load('matlab/cost_map_derv_1_64.txt'));
 
-hold on
-load('traj_0.txt')
-plot(traj_0(2,:),traj_0(1,:));
-
+% figure
+% 
+% [X,Y] = meshgrid([-40:0.800001:40]);
+% Z = cost_map;
+% contour3(X,Y,Z,30)
+% surface(X,Y,Z,'EdgeColor',[.8 .8 .8],'FaceColor','none')
+% grid off
+% Tcam = [135 -70];
+% view(Tcam);
+% colormap cool
+% 
 % hold on
-% load('traj.txt')
-% plot(traj(2,:),traj(1,:));
+% load('traj_0.txt')
+% plot(traj_0(2,:),traj_0(1,:));
