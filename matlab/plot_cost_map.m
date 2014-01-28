e4b22972400941b3e9588760c5378b88009659b7
Jim Mainprice <mainprice@gmail.com>
2014-01-27 19:21:10 -0500

    Work on stomp generated trajectories

--- matlab/plot_cost_map.m
+++ matlab/plot_cost_map.m
@@ -1,4 +1,5 @@
 %clear; clc;
+close all hidden
 
 % cost_map = load('cost_map_0.txt')
 % cost_map = load('cost_map_64.txt');
@@ -8,10 +9,20 @@
 
 % set heat map to be : redbluecmap(10000) or redgreencmap(10000)
 % HeatMap(load('matlab/cost_map_64.txt'));
-HeatMap( load('matlab/cost_map_64.txt'), 'Symmetric', false, 'Colormap', colormap(jet(10000)) );
-HeatMap( load('matlab/cost_map_feat_64.txt'), 'Symmetric', false, 'Colormap', colormap(jet(10000)) );
-HeatMap( load('matlab/cost_map_derv_0_64.txt'), 'Symmetric', false, 'Colormap', colormap(jet(10000)) );
-HeatMap( load('matlab/cost_map_derv_1_64.txt'), 'Symmetric', false, 'Colormap', colormap(jet(10000)) );
+nb_colors = 1000;
+h1 = plot( HeatMap( load('matlab/cost_map_64.txt'), 'Symmetric', false, 'Colormap', colormap(jet(nb_colors)) ));
+h2 = HeatMap( load('matlab/cost_map_feat_64.txt'), 'Symmetric', false, 'Colormap', colormap(jet(nb_colors)) );
+h3 = HeatMap( load('matlab/cost_map_derv_0_64.txt'), 'Symmetric', false, 'Colormap', colormap(jet(nb_colors)) );
+h4 = HeatMap( load('matlab/cost_map_derv_1_64.txt'), 'Symmetric', false, 'Colormap', colormap(jet(nb_colors)) );
+h5 = HeatMap( load('matlab/cost_map_jac_mag_simple.txt'), 'Symmetric', false, 'Colormap', colormap(jet(nb_colors)) );
+h6 = HeatMap( load('matlab/cost_map_jac_mag_custom.txt'), 'Symmetric', false, 'Colormap', colormap(jet(nb_colors)) );
+
+% print(h1, '-dpng', '/home/jmainpri/Pictures/Labmeeting/24.01.14/cost_map_64.png');
+% print(h2, '-dpng', '/home/jmainpri/Pictures/Labmeeting/24.01.14/cost_map_feat_64.png');
+% print(h3, '-dpng', '/home/jmainpri/Pictures/Labmeeting/24.01.14/cost_map_derv_0_64.png');
+% print(h4, '-dpng', '/home/jmainpri/Pictures/Labmeeting/24.01.14/cost_map_derv_1_64.png');
+% print(h5, '-dpng', '/home/jmainpri/Pictures/Labmeeting/24.01.14/cost_map_jac_mag_simple.png');
+% print(h6, '-dpng', '/home/jmainpri/Pictures/Labmeeting/24.01.14/cost_map_jac_mag_custom.png');
 
 % figure
 % 
