97a5f26a9b2bb29c89333d0c9d941b3badfa12b9
Rafi Hayne <rafi@field-test.(none)>
2014-05-28 16:46:19 -0400

    fix justin launch

--- launch_files/launch_justin_kitchen.sh
+++ launch_files/launch_justin_kitchen.sh
@@ -1,2 +1,2 @@
 #!/bin/bash  
-move3d-qt-studio -launch Diffusion -setgui -params ../move3d-launch/parameters/params_justin_kitchen -c pqp -f ../../move../../assets/CostHriFunction/Justin/JustinTable_ICRA_baseFixed2.p3d
+move3d-qt-studio -launch Diffusion -setgui -params ../move3d-launch/parameters/params_justin_kitchen -c pqp -f ../../assets/CostHriFunction/Justin/JustinTable_ICRA_baseFixed2.p3d
