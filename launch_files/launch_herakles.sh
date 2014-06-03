350468c7c2fba70ac30400b845db30c5b2640227
Jim Mainprice <mainprice@gmail.com>
2014-06-03 14:47:51 -0400

    Add herakles launch file

---
+++ launch_files/launch_herakles.sh
@@ -0,0 +1,3 @@
+move3d-qt-studio -setgui -c pqp -f ../../assets/Achile/HumanTestNoGraphics.p3d  -params ../move3d-launch/parameters/params_human
+# -sc ../../assets/Collaboration/SCENARIOS/collaboration_test_reach.sce
+# -launch HumanPlanning 
