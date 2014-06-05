1fc8e0de56f2f0dce27d35467264c49129b3f9d8
Jim Mainprice <mainprice@gmail.com>
2014-06-04 20:13:25 -0400

    work on human ioc

--- launch_files/launch_collaboration_planning.sh
+++ launch_files/launch_collaboration_planning.sh
@@ -1,1 +1,2 @@
-move3d-qt-studio -launch HumanPlanning -setgui -c pqp -f ../../assets/Collaboration/TwoHumansPlanning.p3d  -params ../move3d-launch/parameters/params_collaboration_planning -sc ../../assets/Collaboration/SCENARIOS/collaboration_test_reach.sce
+move3d-qt-studio -setgui -c pqp -f ../../assets/Collaboration/TwoHumansPlanning.p3d  -params ../move3d-launch/parameters/params_collaboration_planning -sc ../../assets/Collaboration/SCENARIOS/collaboration_test_reach.sce
+# -launch HumanPlanning 
--- launch_files/launch_voxel_occupancy.sh
+++ launch_files/launch_voxel_occupancy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash  
-move3d-qt-studio -launch VoxelOccupancy -setgui -params ../move3d-launch/parameters/params_voxel_occupancy -c pqp -f ../../move../../assets/Gesture/HumanTestM.p3d -sc ../../move../../assets/Gesture/SCENARIOS/Human.sce
+move3d-qt-studio -launch VoxelOccupancy -setgui -params ../move3d-launch/parameters/params_voxel_occupancy -c pqp -f ../../assets/Gesture/HumanTestM.p3d -sc ../../assets/Gesture/SCENARIOS/Human.sce
 # -launch PredictionSimulation
