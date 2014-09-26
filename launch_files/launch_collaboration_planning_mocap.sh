c87a4fdbfa4eb5c43910f727bb45e85d78ccba2b
Jim Mainprice <mainprice@gmail.com>
2014-09-26 19:25:42 -0400

    Add new scenario

--- launch_files/launch_collaboration_planning_mocap.sh
+++ launch_files/launch_collaboration_planning_mocap.sh
@@ -11,5 +11,5 @@ do
 done
 
 $debug  move3d-qt-studio -setgui -c pqp -f ../../assets/Collaboration/TwoHumansTableMocap.p3d  -params ../move3d-launch/parameters/params_collaboration_planning_mocap -sc ../../assets/Collaboration/SCENARIOS/collaboration_test_mocap.sce 
-# -launch HumanPlanning 
+# -nogui -launch RunIOC  
 
