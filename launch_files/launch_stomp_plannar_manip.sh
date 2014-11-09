7abd2cca6314b12a0a28dd92c2b1162b6010692b
Jim Mainprice <mainprice@gmail.com>
2014-11-09 00:33:01 -0500

    Work on goal regions stomp

--- launch_files/launch_stomp_plannar_manip.sh
+++ launch_files/launch_stomp_plannar_manip.sh
@@ -1,1 +1,2 @@
-move3d-qt-studio -launch runStomp  -setgui -params ../move3d-launch/parameters/params_stomp_plannar_manip -c pqp -f ../../assets/CostDistanceKCD/3dof/manipulator.p3d -sc ../../assets/CostDistanceKCD/3dof/SCENARIO/manip.sce
+move3d-qt-studio -launch runStomp  -setgui -params ../move3d-launch/parameters/params_stomp_plannar_manip -c pqp -f ../../assets/CostDistanceKCD/3dof/manipulator.p3d -sc ../../assets/CostDistanceKCD/3dof/SCENARIO/manip_stomp.sce
+# manip_stomp.sce or manip.sce
