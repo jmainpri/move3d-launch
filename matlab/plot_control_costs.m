c4b5cee3e6ca8f3a8c8c903045a295fdb1e4e0eb
Jim Mainprice <mainprice@gmail.com>
2015-08-05 15:05:15 +0200

    Remove weights

--- matlab/plot_control_costs.m
+++ matlab/plot_control_costs.m
@@ -6,28 +6,28 @@ figure
 % number of dofs
 % PR2 : 7
 % Human : 22
-K = 6; 
+% Stones : 2
+K = 7; 
 
 % M is the number of rows
 % N is the number of plots per row
-M = 6;
-N = K / M;
+M = 3; % 6
+N = floor(K / M);
 if mod(M,N) >= 1,
     N = N +1;
 end
-
 
 folder = '/jim_local/Dropbox/move3d/move3d-launch/launch_files/control_cost_profiles/';
 
 for k=0:K-1,
     subplot(M,N,k+1)
     id = sprintf('%3.3d',k);
-    file = [folder 'stomp_vel_' id '.txt'];
+    file = [folder 'stomp_vel_' id '.csv'];
+    %file = [folder 'stomp_acc_' id '.csv'];
+    %file = [folder 'stomp_jerk_' id '.csv'];
     display(['load : ' file]);
     mat = csvread(file);
     size(mat)
-    % mat = csvread([folder 'stomp_acc_' id '.txt']);
-    % mat = csvread([folder 'stomp_jerk_' id '.txt']);
     control_costs = mat;
     plot( control_costs );
     % title(['DOF ' id]);
