dc530b06446576184c44b01b3032bfb881d137ae
Jim Mainprice <mainprice@gmail.com>
2015-08-06 11:51:18 +0200

    Fix pr2 human scenarii

--- launch_files/launch_pr2_human_9.sh
+++ launch_files/launch_pr2_human_9.sh
@@ -1,2 +1,2 @@
 #!/bin/bash  
-move3d-qt-studio -launch runStomp -setgui -params ../move3d-launch/parameters/params_pr2_human_9 -c pqp -f ../../move../../assets/CostHriFunction/PR2/Pr2_ShelfAndHuman.p3d -sc ../../move../../assets/CostHriFunction/PR2/SCENARIOS/Pr2ShelfAndHumanTmp9.sce
+move3d-qt-studio -launch runStomp -setgui -params ../move3d-launch/parameters/params_pr2_human_9 -c pqp -f ../../move3d-launch/../assets/CostHriFunction/PR2/Pr2_ShelfAndHuman.p3d -sc ../../move3d-launch/../assets/CostHriFunction/PR2/SCENARIOS/Pr2ShelfAndHumanTmp9.sce
